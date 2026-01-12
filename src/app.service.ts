e7e539de2a3905c8a312d828f1de0eb7570fdacb
Derrick Ngigi <ngigid0@gmail.com>
2026-01-12 21:18:26 +0300

    update docs

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Welcome to the backend submission for the Health Tech Solutions technical assignment project! Go to /api/docs to explore the API documentation.';
   }
 }
